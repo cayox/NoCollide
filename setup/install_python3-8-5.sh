393cd7685f4391c04170aab37ccb2d03f7f680a0
cayox <n.p.98@hotmail.de>
2020-11-04 20:53:53 +0100

    extend python install shell script fix typo in requirements

--- setup/install_python3-8-5.sh
+++ setup/install_python3-8-5.sh
@@ -1,7 +1,11 @@
 # download python 3.8.5
 wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
 # unpack it
-tar xf Python-3.8.5.tar.xz
+tar xf Python-3.8.5.tgz
+# install build libraries
+sudo apt install libssl-dev
+# delete tarball
+rm -r Python-3.8.5.tgz
 # move into python dir 
 cd ./Python-3.8.5
 # configure python
@@ -16,4 +20,4 @@ sudo make altinstall
 # echo "alias pip=pip3.8" >> ~/.bashrc
 # source ~/.bashrc
 
-echo you can us python 3.8.5 by typing "python3.8" instead of regular "python" command
+echo you can use python 3.8.5 by typing "python3.8" instead of regular "python" command
